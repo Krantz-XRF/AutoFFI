d7aa34b21acd4f813ed62b749344b65fbe0e9b17
Krantz-XRF <Krantz.XRF@outlook.com>
2020-05-31 00:37:01 +0800

    Add: detect name conflict with Haskell keywords

--- src/config.cpp
+++ src/config.cpp
@@ -1,5 +1,7 @@
 #include "config.h"
 
+#include <set>
+
 #include <fmt/ranges.h>
 #include <spdlog/spdlog.h>
 
@@ -27,27 +29,38 @@ bool ffi::validate_config(const config& cfg, spdlog::logger& logger) {
   return true;
 }
 
+namespace {
+const std::set<std::string_view> haskell_keywords{
+    "as",        "case",   "class",  "data",    "default", "deriving",
+    "do",        "else",   "forall", "foreign", "hiding",  "if",
+    "in",        "import", "infix",  "infixl",  "infixr",  "instance",
+    "let",       "mdo",    "module", "newtype", "of",      "proc",
+    "qualified", "rec",    "then",   "type",    "where",
+};
+}  // namespace
+
 int ffi::name_clashes(const name_resolver::rev_name_map& m,
                       spdlog::logger& logger, std::string_view kind,
                       std::string_view scope) {
-  const auto n = m.bucket_count();
   auto conflict{0};
-  for (size_t i = 0; i < n; ++i) {
-    if (m.bucket_size(i) <= 1) continue;
-    for (auto it = m.cbegin(i); it != m.cend(i);) {
-      const auto cur = it->first;
-      if (next(it)->first != cur)
-        ++it;
-      else {
-        fmt::memory_buffer buf;
-        format_to(buf,
-                  "the following {} names in '{}' all converts to '{}':", kind,
-                  scope, it->first);
-        for (; it->first == cur; ++it) format_to(buf, "\n- {}", it->second);
-        logger.error(to_string(buf));
-        ++conflict;
-      }
+  for (auto it = begin(m); it != end(m);) {
+    const auto cur = it->first;
+    const bool key_clash = haskell_keywords.find(cur) != end(haskell_keywords);
+    // no clash
+    if (const auto n = next(it);
+        !key_clash && (n == end(m) || n->first != cur)) {
+      ++it;
+      continue;
     }
+    // clash with a keyword or with each other
+    fmt::memory_buffer buf;
+    format_to(buf, "the following {} names in '{}' all convert to '{}':", kind,
+              scope, cur);
+    for (; it != end(m) && it->first == cur; ++it)
+      format_to(buf, "\n- {}", it->second);
+    logger.error(to_string(buf));
+    if (key_clash) logger.info("'{}' is a Haskell keyword.", cur);
+    ++conflict;
   }
   return conflict;
 }
